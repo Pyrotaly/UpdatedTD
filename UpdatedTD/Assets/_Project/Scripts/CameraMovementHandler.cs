210fcf0d0af13ae9884836f58954539dd8901ff0
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2023-06-09 00:41:45 -0400

    MadeCameraWork

--- UpdatedTD/Assets/_Project/Scripts/CameraMovementHandler.cs
+++ UpdatedTD/Assets/_Project/Scripts/CameraMovementHandler.cs
@@ -1,21 +1,70 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+//TODO : Make camera only move when player presses a cetain button, this will allow player to press UI in corner without moving camera
 
 namespace UpdatedTD
 {
     public class CameraMovementHandler : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] private float edgeSize = 10f;
+        [SerializeField] private float cameraMoveSpeed = 10f;
+        [SerializeField] private float zoomChangeSpeed = 10f;
+        [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+        public float targetOrthographicSize;
+        public float orthographicSizeMin;
+        public float orthographicSizeMax;
+
+        private void Update()
         {
-        
+            HandleEdgeScrolling();
+            HandleZoom();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void HandleEdgeScrolling()
         {
-        
+            Vector3 inputDir = new Vector3(0, 0, 0);
+
+            if (Input.mousePosition.x > Screen.width - edgeSize)
+            {
+                inputDir.x = 1f;
+            }
+            if (Input.mousePosition.x < edgeSize)
+            {
+                inputDir.x = -1f;
+            }
+            if (Input.mousePosition.y > Screen.height - edgeSize)
+            {
+                inputDir.y = 1f;
+            }
+            if (Input.mousePosition.y < edgeSize)
+            {
+                inputDir.y = -1f;
+            }
+
+
+            Vector3 moveDir = transform.up * inputDir.y + transform.right * inputDir.x;
+
+            transform.position += moveDir * cameraMoveSpeed * Time.deltaTime;
+        }
+
+        private void HandleZoom()
+        {
+            if (Input.mouseScrollDelta.y > 0)
+            {
+                targetOrthographicSize -= 0.4f;
+            }
+            if (Input.mouseScrollDelta.y < 0)
+            {
+                targetOrthographicSize += 0.4f;
+            }
+
+            targetOrthographicSize = Mathf.Clamp(targetOrthographicSize, orthographicSizeMin, orthographicSizeMax);
+            cinemachineVirtualCamera.m_Lens.OrthographicSize =
+                Mathf.Lerp(cinemachineVirtualCamera.m_Lens.OrthographicSize, targetOrthographicSize, Time.deltaTime * zoomChangeSpeed);
         }
     }
 }
