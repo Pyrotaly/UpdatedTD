64a76da68c957a7cc507947f2d620f560047f718
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2023-07-24 09:59:37 -0400

    Moving Files Around, workin on save systewm

--- save-system/Runtime/DataPersistence/Data/GameData.cs
+++ save-system/Runtime/DataPersistence/Data/GameData.cs
@@ -7,7 +7,12 @@ namespace GenericSave
     [System.Serializable]
     public class GameData
     {
-        public long lastUpdated;
+        public long lastUpdated; //time to know when last time save or whatever WIP
+
+
+
+
+        //TODO : Remove 
         public int deathCount;
         public Vector3 playerPosition;
         public SerializableDictionary<string, bool> coinsCollected;
@@ -17,33 +22,36 @@ namespace GenericSave
         // the game starts with when there's no data to load
         public GameData()
         {
+
+
+            //TODO : Remove
             this.deathCount = 0;
             playerPosition = Vector3.zero;
             coinsCollected = new SerializableDictionary<string, bool>();
             playerAttributesData = new AttributesData();
         }
 
-        
-        //TODO : Use this for games that would have percentage completion stat
-        //public int GetPercentageComplete()
-        //{
-        //    // figure out how many coins we've collected
-        //    int totalCollected = 0;
-        //    foreach (bool collected in coinsCollected.Values)
-        //    {
-        //        if (collected)
-        //        {
-        //            totalCollected++;
-        //        }
-        //    }
 
-        //    // ensure we don't divide by 0 when calculating the percentage
-        //    int percentageCompleted = -1;
-        //    if (coinsCollected.Count != 0)
-        //    {
-        //        percentageCompleted = (totalCollected * 100 / coinsCollected.Count);
-        //    }
-        //    return percentageCompleted;
-        //}
+        //TODO : Use this for games that would have percentage completion stat
+        public int GetPercentageComplete()
+        {
+            // figure out how many coins we've collected
+            int totalCollected = 0;
+            foreach (bool collected in coinsCollected.Values)
+            {
+                if (collected)
+                {
+                    totalCollected++;
+                }
+            }
+
+            // ensure we don't divide by 0 when calculating the percentage
+            int percentageCompleted = -1;
+            if (coinsCollected.Count != 0)
+            {
+                percentageCompleted = (totalCollected * 100 / coinsCollected.Count);
+            }
+            return percentageCompleted;
+        }
     }
 }
