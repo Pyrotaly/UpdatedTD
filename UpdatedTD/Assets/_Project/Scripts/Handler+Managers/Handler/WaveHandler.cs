050d40a2bbfabe648c0b7b3c0563e3cd612eb0bb
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2023-08-01 23:51:22 -0400

    checkpoint

--- UpdatedTD/Assets/_Project/Scripts/Handler+Managers/Handler/WaveHandler.cs
+++ UpdatedTD/Assets/_Project/Scripts/Handler+Managers/Handler/WaveHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,57 @@ namespace UpdatedTD
 {
     public class WaveHandler : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [Header("References")]
+        [SerializeField] private GameObject[] enemyPrefabs;
+
+        [Header("Attributes")]
+        [SerializeField] private int baseEnemies = 8;
+        [SerializeField] private float enemiesPerSecond = 0.5f;
+        [SerializeField] private float timeBetweenWaves = 5f;
+        [SerializeField] private float difficulityScalingFactor = 0.75f;
+
+        private int currentWave = 1;
+        private float timeSinceLastSpawn;
+        private int enemiesAlive;
+        private int enemiesLeftToSpawn;
+        private bool isSpawning = false;
+
+        //TODO : Start wave if player does somthing
+        private void Start()
         {
-        
+            StartWave();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
-        
+            if (!isSpawning) return;
+
+            timeSinceLastSpawn += Time.deltaTime;
+
+            if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0)
+            {
+                SpawnEnemy();
+                enemiesLeftToSpawn--;
+                enemiesAlive++;
+                timeSinceLastSpawn = 0f;
+            }
+        }
+
+        private void SpawnEnemy()
+        {
+            GameObject prefabToSpawn = enemyPrefabs[0];
+            //Instantiate(prefabToSpawn)
+        }
+
+        private void StartWave()
+        {
+            isSpawning = true;
+            enemiesLeftToSpawn = EnemiesPerWave();
+        }
+
+        private int EnemiesPerWave()
+        {
+            return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficulityScalingFactor));
         }
     }
 }
